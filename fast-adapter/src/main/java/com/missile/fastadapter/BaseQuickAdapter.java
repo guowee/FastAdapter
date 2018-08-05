0e9d93fab3a101c7f733333cfcd91f76a88ee5e4
Guowee <meditator_guo@163.com>
2018-08-05 12:30:46 +0800

    update to jcenter

--- fast-adapter/src/main/java/com/missile/fastadapter/BaseQuickAdapter.java
+++ fast-adapter/src/main/java/com/missile/fastadapter/BaseQuickAdapter.java
@@ -10,7 +10,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
